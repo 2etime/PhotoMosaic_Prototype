705e64e29ffa561f70c07eebc19c888296546cb3
Rick Twohy <twohyjr@gmail.com>
2019-11-12 18:42:39 -0500

    Initial Commit

---
+++ PhotoMosaic_Prototype/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotoMosaic_Prototype
+//
+//  Created by Rick Twohy Jr on 11/12/19.
+//  Copyright © 2019 2etime. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
